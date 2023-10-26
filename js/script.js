fab223bd6fa2562675913a924e11924fa4aa9dcb
t0rch13 <batyrhan22.11@gmail.com>
2023-10-27 00:36:35 +0600

    book page

--- js/script.js
+++ js/script.js
@@ -32,16 +32,23 @@ function displayGenre(genre) {
 const books = [
     { title: "War and Peace", author: "Lev Tolstoy" },
     { title: "Brief History of Time", author: "Stephen Hawking" },
-    { title: "1984", author: "John Orwell" }
+    { title: "1984", author: "George Orwell" },
+    { title: "Fight Club", author: "Chuck Palahniuk" },
+    { title: "Capital", author: "Karl Marx"}
 ];
 
 function displayAllBooks() {  
     const bookList = document.getElementById("booksList");
 
-    books.forEach((book) => {
+    books.forEach((book, index) => {
         const listItem = document.createElement("li");
+        const link = document.createElement("a");
+        link.href = `../html/book.html?index=${index}`; 
+        link.textContent = `Title: ${book.title}, Author: ${book.author}`;
         listItem.className = "list-group-item list-group-item-warning";
-        listItem.textContent = `Title: ${book.title}, Author: ${book.author}`;
+        link.style.textDecoration = "none"; 
+        link.style.color = "black";
+        listItem.appendChild(link);
         bookList.appendChild(listItem);
     });
 }
